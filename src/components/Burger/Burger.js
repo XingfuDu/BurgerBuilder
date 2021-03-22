7d5c8b076f8c8ad9d04528802daef3ff9873bab2
duxingfu <du.xingf@northeastern.edu>
2021-03-21 22:22:48 -0400

    add ingredient buttons

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,17 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'; 
 
 const burger = (props)=> {
-    const transformedIngredients = Object.keys(props.ingredient)
+    let transformedIngredients = Object.keys(props.ingredient)
         .map((igKey)=>{
             return [...Array(props.ingredient[igKey])].map((_,i)=>{ 
                 return <BurgerIngredient key={igKey+i} type={igKey}/>
             });
-        });
+        }).reduce((arr,el)=>{
+            return arr.concat(el);
+        },[]);
+    if(transformedIngredients.length===0) {
+        transformedIngredients=<p>Please start add ingredients!</p>
+    }
     return  (
         <div className={classes.Burger}>
            <BurgerIngredient type="bread-top"/> 
--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -26,8 +26,8 @@ class BurgerIngredient extends Component {
 			case 'salad':
 				ingredient = <div className={classes.Salad} />;
 				break;
-			case 'beacon':
-				ingredient = <div className={classes.Beacon} />;
+			case 'bacon':
+				ingredient = <div className={classes.Bacon} />;
 				break;
 			default:
 				ingredient = null;
