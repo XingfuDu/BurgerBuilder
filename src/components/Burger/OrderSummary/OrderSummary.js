553d64732df614378e16ea92d9e87c0cab77f53a
duxingfu <du.xingf@northeastern.edu>
2021-03-22 22:26:38 -0400

    add navigation

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
 	const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
@@ -14,12 +15,15 @@ const orderSummary = (props) => {
 		<Aux>
 			<h3>Your Order</h3>
 			<p>A delicious burger with the following ingredients</p>
-			<ul>
-                {ingredientSummary}
-            </ul>
-            <p>Continue to Checkout?</p>
-            <button>CANCEL</button>
-            <button>CONTINUE</button>
+			<ul>{ingredientSummary}</ul>
+			<p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+			<p>Continue to Checkout?</p>
+			<Button btnType="Danger" clicked={props.purchaseCancelled}>
+				CANCEL
+			</Button>
+			<Button btnType="Success" clicked={props.purchaseContinued}>
+				CONTINUE
+			</Button>
 		</Aux>
 	);
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,6 +74,10 @@ class BurgerBuilder extends Component {
 		this.setState({ purchasing: false });
 	};
 
+	purchaseContinueHandler = () => {
+		alert('You continue!');
+	};
+
 	render() {
 		const disabledInfo = {
 			...this.state.ingredients
@@ -84,7 +88,12 @@ class BurgerBuilder extends Component {
 		return (
 			<Aux>
 				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-					<OrderSummary ingredients={this.state.ingredients} />
+					<OrderSummary
+						ingredients={this.state.ingredients}
+						price={this.state.totalPrice}
+						purchaseCancelled={this.purchaseCancelHandler}
+						purchaseContinued={this.purchaseContinueHandler}
+					/>
 				</Modal>
 				<Burger ingredient={this.state.ingredients} />
 				<BuildControls
