c80fa6cb715b90d9cf5f17c1a85249190de61d96
duxingfu <du.xingf@northeastern.edu>
2021-03-24 20:53:51 -0400

    add server acess through axios

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
+
 class App extends Component {
+
 	render() {
 		return (
 			<div>
