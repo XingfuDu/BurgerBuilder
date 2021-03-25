9c30726997447097d1bd6119e7207c6bbae5000f
duxingfu <du.xingf@northeastern.edu>
2021-03-24 20:53:16 -0400

    add access to servers through axios

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,17 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.module.css';
 import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
-
 	shouldComponentUpdate(nextProps, nextState) {
-		return nextProps.show!==this.props.show;
+		return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
 	}
 	componentDidUpdate() {
 		console.log('[Modal.js] did update');
 	}
-	
+
 	render() {
 		return (
 			<Aux>
@@ -27,7 +26,7 @@ class Modal extends Component {
 				</div>
 			</Aux>
 		);
-	};
-};
+	}
+}
 
 export default Modal;
